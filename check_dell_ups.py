d704d7f51bd436db2305b3bcb46f3ab151a9b13a
luunguyen <luunguyen@ccintegration.com>
2016-10-31 11:30:11 +0700

    Update

--- check_dell_ups.py
+++ check_dell_ups.py
@@ -8,15 +8,15 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 from pysnmp.hlapi import *
 
-def main():
 
+def main():
 	try:
 			# Starting check_mge_eaton_ups.py
 		iterator = getCmd(SnmpEngine(),
 						  CommunityData('public'),
 						  UdpTransportTarget(('172.16.4.71', 161)),
 						  ContextData(),
-						  ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+						  ObjectType(ObjectIdentity('SNMPv2-MIB', '1.3.6.1.2.1.33.1.2.4.0', 0)))
 
 		errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
 
