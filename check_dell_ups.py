1738e2f2930e24d2f347506f63f69de1756ee738
luunguyen <luunguyen@ccintegration.com>
2016-10-31 11:32:29 +0700

    Update

--- check_dell_ups.py
+++ check_dell_ups.py
@@ -11,7 +11,7 @@ from pysnmp.hlapi import *
 
 def main():
 	try:
-			# Starting check_mge_eaton_ups.py
+		# Starting check_mge_eaton_ups.py
 		iterator = getCmd(SnmpEngine(),
 						  CommunityData('public'),
 						  UdpTransportTarget(('172.16.4.71', 161)),
@@ -30,14 +30,12 @@ def main():
 					print(' = '.join([x.prettyPrint() for x in varBind]))
 
 	except IOError, msg:
-		nagiosOutput(3, "IOError")
 		raise
 	except Exception, msg:
-		nagiosOutput(3, "Exception")
 		raise
 
 
 if __name__ == "__main__":
-		main()
+	main()
 
 
