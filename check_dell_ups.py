4c329c084f548f2514e4829bb46feef35408ec7b
luunguyen <luunguyen@ccintegration.com>
2016-10-31 11:51:06 +0700

    Update

--- check_dell_ups.py
+++ check_dell_ups.py
@@ -5,16 +5,20 @@ import subprocess
 
 
 def main():
-   print get_capacity_dell_ups()
+   dell_c = get_capacity_dell_ups()
+   if dell_c == "100":
+      print "OK"
+   else:
+      print "NG"
 
 
 def get_capacity_dell_ups():
- try:
+  try:
      cmd = ["snmpwalk", "-c", "public", "-v", "1", "172.16.4.71", "1.3.6.1.2.1.33.1.2.4.0","-O", "q", "-O", "v"]
      dell_capacity = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
      return dell_capacity.strip('\n')
- except:
-   return "error"
+  except:
+     return "error"
 
 if __name__ == "__main__":
     main()
